cdbab34e0f6c0c3ccf16c2f76684f6c369c0798d
Mohammad Mahdi Shokor <mohammadmahdishokor@gmail.com>
2025-12-07 18:39:36 +0200

    Fixed bugs in competition Screen - mobile

--- lib/screens/competition_screen_mobile.dart
+++ lib/screens/competition_screen_mobile.dart
@@ -344,6 +344,7 @@ class _CompetitionScreenState extends State<CompetitionScreenMobile>
                                         : null,
                                 child: Text(
                                   answer,
+                                  textAlign: TextAlign.center,
                                   style: GoogleFonts.aBeeZee(
                                     fontWeight: FontWeight.w500,
                                     fontSize: 18,
